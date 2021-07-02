b66a30d50a51df23e5fbcaa6807fbb9abf1f72de
NNydek <nydeous17@gmail.com>
2021-07-02 13:46:08 +0200

    clang format

--- day02/main.cpp
+++ day02/main.cpp
@@ -16,98 +16,94 @@ std::string readFile(std::string filename) {
 }
 
 int part1() {
-    std::string filename("input.txt");
-    std::string input;
-    input = readFile(filename);
+  std::string filename("input.txt");
+  std::string input;
+  input = readFile(filename);
 
-    int length, width, height;
-    std::string ch = "x";
-    std::size_t pos = 0;
+  int length, width, height;
+  std::string ch = "x";
+  std::size_t pos = 0;
 
-    int flag = 0;
-    int surfaceArea = 0;
-    int temp = 0;
-    int result = 0;
+  int flag = 0;
+  int surfaceArea = 0;
+  int temp = 0;
+  int result = 0;
 
-    while ((pos = input.find(ch)) != std::string::npos) {
-        if (flag == 0) {
-            length = stoi(input.substr(0, pos));
-            input.erase(0, pos + ch.length());
-            flag = 1;
-        }
-        else if (flag == 1) {
-            width = stoi(input.substr(0, pos));
-            input.erase(0, pos + ch.length());
-            flag = 2;
-        }
-        else if (flag == 2) {
-            height = stoi(input.substr(0, pos));
-            input.erase(0, pos + ch.length());
-            flag = 0;
+  while ((pos = input.find(ch)) != std::string::npos) {
+    if (flag == 0) {
+      length = stoi(input.substr(0, pos));
+      input.erase(0, pos + ch.length());
+      flag = 1;
+    } else if (flag == 1) {
+      width = stoi(input.substr(0, pos));
+      input.erase(0, pos + ch.length());
+      flag = 2;
+    } else if (flag == 2) {
+      height = stoi(input.substr(0, pos));
+      input.erase(0, pos + ch.length());
+      flag = 0;
 
-            surfaceArea += 2 * (length * width + width * height + height * length);
-            temp = std::max({ length, width, height });
+      surfaceArea += 2 * (length * width + width * height + height * length);
+      temp = std::max({length, width, height});
 
-            if (length == temp)
-                length = 1;
-            else if (width == temp)
-                width = 1;
-            else if (height == temp)
-                height = 1;
-            result += length * width * height;
-        }
+      if (length == temp)
+        length = 1;
+      else if (width == temp)
+        width = 1;
+      else if (height == temp)
+        height = 1;
+      result += length * width * height;
     }
-    result += surfaceArea;
-    return result;
+  }
+  result += surfaceArea;
+  return result;
 }
 
 int part2() {
-    std::string filename("input.txt");
-    std::string input;
-    input = readFile(filename);
+  std::string filename("input.txt");
+  std::string input;
+  input = readFile(filename);
 
-    int length, width, height;
-    std::string ch = "x";
-    std::size_t pos = 0;
+  int length, width, height;
+  std::string ch = "x";
+  std::size_t pos = 0;
 
-    int flag = 0;
-    int surfaceArea = 0;
-    int temp = 0;
-    int result = 0;
+  int flag = 0;
+  int surfaceArea = 0;
+  int temp = 0;
+  int result = 0;
 
-    while ((pos = input.find(ch)) != std::string::npos) {
-        if (flag == 0) {
-            length = stoi(input.substr(0, pos));
-            input.erase(0, pos + ch.length());
-            flag = 1;
-        }
-        else if (flag == 1) {
-            width = stoi(input.substr(0, pos));
-            input.erase(0, pos + ch.length());
-            flag = 2;
-        }
-        else if (flag == 2) {
-            height = stoi(input.substr(0, pos));
-            input.erase(0, pos + ch.length());
-            flag = 0;
+  while ((pos = input.find(ch)) != std::string::npos) {
+    if (flag == 0) {
+      length = stoi(input.substr(0, pos));
+      input.erase(0, pos + ch.length());
+      flag = 1;
+    } else if (flag == 1) {
+      width = stoi(input.substr(0, pos));
+      input.erase(0, pos + ch.length());
+      flag = 2;
+    } else if (flag == 2) {
+      height = stoi(input.substr(0, pos));
+      input.erase(0, pos + ch.length());
+      flag = 0;
 
-            surfaceArea += length * width * height;
-            temp = std::max({ length, width, height });
+      surfaceArea += length * width * height;
+      temp = std::max({length, width, height});
 
-            if (length == temp)
-                length = 0;
-            else if (width == temp)
-                width = 0;
-            else if (height == temp)
-                height = 0;
-            result += 2 * (length + width + height);
-        }
+      if (length == temp)
+        length = 0;
+      else if (width == temp)
+        width = 0;
+      else if (height == temp)
+        height = 0;
+      result += 2 * (length + width + height);
     }
-    result += surfaceArea;
-    return result;
+  }
+  result += surfaceArea;
+  return result;
 }
 
 int main() {
-    std::cout << "Part 1 answer: " << part1() 
-        << "\nPart 2 answer: " << part2() << "\n";
+  std::cout << "Part 1 answer: " << part1() << "\nPart 2 answer: " << part2()
+            << "\n";
 }
