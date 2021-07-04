a284c72b77041e32987654ef334608b5c03e6b92
NNydek <nydeous17@gmail.com>
2021-07-04 20:21:17 +0200

    add solution day04

---
+++ day04/main.cpp
@@ -0,0 +1,26 @@
+#include "md5.h"
+
+int part1(std::string input) {
+  int result = 0;
+
+  while (md5(input + std::to_string(++result)).compare(0, 5, "00000") != 0)
+    ;
+
+  return result;
+}
+
+int part2(std::string input) {
+  int result = 0;
+
+  while (md5(input + std::to_string(++result)).compare(0, 6, "000000") != 0)
+    ;
+
+  return result;
+}
+
+int main() {
+  std::string input = "bgvyzdsv";
+
+  std::cout << "Part 1 answer: " << part1(input)
+            << "\nPart 2 answer: " << part2(input) << '\n';
+}
